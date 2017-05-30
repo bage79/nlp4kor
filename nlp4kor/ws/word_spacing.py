36c72659cf9e81f885028a6ac96bcfeffdf7b9ba
Hyewoong Park <bage79@gmail.com>
2017-05-30 19:27:00 +0900

    tensorflow running OK.

--- nlp4kor/ws/word_spacing.py
+++ nlp4kor/ws/word_spacing.py
@@ -2,6 +2,7 @@ import gzip
 import os
 
 import numpy as np
+import tensorflow as tf
 
 from bage_utils.base_util import is_my_pc
 from bage_utils.datafile_util import DataFileUtil
@@ -38,22 +39,7 @@ class WordSpacing(object):
         return d
 
     @classmethod
-    def chars2vector(cls, features, labels, features_vector, labels_vector):
-        _features, _labels = [], []
-        for chars, has_space in zip(features, labels):
-            chars = list([c for c in chars])  # two characters.
-            chars_v = features_vector.to_vectors(chars)
-            chars_v = list([v for v in chars_v])
-            feature = np.concatenate(chars_v)  # concated feature
-            label = labels_vector.to_vector(has_space)
-
-            # log.info('%s, %s = %s (len=%s) -> %s (len=%s)' % (chars[0], chars[1], feature, len(feature), label, len(label)))
-            _features.append(feature)
-            _labels.append(label)
-        return _features, _labels
-
-    @classmethod
-    def sentence2features_labels(cls, sentence, left_gram=2, right_gram=2):
+    def sentence2features_labels(cls, sentence, left_gram=2, right_gram=2) -> (list, list):
         if left_gram < 1 or right_gram < 1:
             return [], []
 
@@ -75,7 +61,7 @@ class WordSpacing(object):
         features = []
         for i in range(left_gram, len(no_space_sentence) - right_gram + 1):
             a, b = no_space_sentence[i - left_gram: i], no_space_sentence[i: i + right_gram]
-            features.append((a + b))
+            features.append(a + b)
             # log.debug('[%d] "%s" "%s" %s' % (i-2, a, b, labels[i-2]))
         return features, labels.tolist()
 
@@ -106,6 +92,7 @@ class WordSpacing(object):
 
 if __name__ == '__main__':
     left_gram, right_gram = 2, 2
+    ngram = left_gram + right_gram
     sentences_file = SENTENCES_FILE_KO_WIKIPEDIA_ORG
 
     log.info('learning...')
@@ -132,17 +119,16 @@ if __name__ == '__main__':
     labels_vector = OneHotVector([0, 1])  # 붙여쓰기=0, 띄어쓰기=1
     log.info('load characters list OK. len: %s\n' % NumUtil.comma_str(len(features_vector)))
 
-    # log.info('dataset_file: %s' % dataset_file)
-
     if is_my_pc():
-        max_len = 10000  # small
-        dataset_file = os.path.join(DATA_DIR_KO_WIKIPEDIA_ORG, 'ko.wikipedia.org.dataset.10k.gz')
+        max_len = 100  # small
+        dataset_file = os.path.join(DATA_DIR_KO_WIKIPEDIA_ORG, 'ko.wikipedia.org.dataset.%d.left=%d.right=%d.gz' % (max_len, left_gram, right_gram))
     else:
         max_len = 0  # full
         dataset_file = os.path.join(DATA_DIR_KO_WIKIPEDIA_ORG, 'ko.wikipedia.org.dataset.gz')
 
-    log.info('create dataset...')
     if not os.path.exists(dataset_file):
+        log.info('create dataset...')
+        log.info('dataset_file: %s' % dataset_file)
         features, labels = [], []
         total = FileUtil.count_lines(sentences_file, gzip_format=True)
         check_interval = 10000 if not is_my_pc() else 1
@@ -169,8 +155,28 @@ if __name__ == '__main__':
         log.info('create dataset OK.\n')
 
     log.info('dataset load...')
-    dataset = DataSet.load(dataset_file)
+    dataset = DataSet.load(dataset_file, to_one_hot_vector=True)
+    log.info(dataset)
     log.info('dataset load OK.\n')
+
+    log.info('check samples...')
+    for i, (features_batch, labels_batch) in enumerate(dataset.next_batch(batch_size=10), 1):
+        if i > 3:
+            break
+        for a, b in zip(features_batch, labels_batch):
+            if dataset.is_one_hot_vector:
+                feature, label = a, b
+                _feature = feature.reshape((ngram, len(features_vector)))
+                chars = ''.join(features_vector.to_values(_feature))
+                has_space = np.argmax(label)
+                log.info('[%s] %s -> %s, %s (len=%s) %s (len=%s)' % (i, chars, label, feature, len(feature), label, len(label)))
+            else:
+                chars, has_space = a, b
+                chars_v = features_vector.to_vectors(chars)  # vectors of two characters
+                feature = np.concatenate(chars_v)  # concated feature
+                label = labels_vector.to_vector(has_space)
+                log.info('[%s] %s -> %s, %s (len=%s) %s (len=%s)' % (i, chars, label, feature, len(feature), label, len(label)))
+    log.info('check samples OK.\n')
 
     log.info('split to train, test, validation...')
     datasets = DataSets.to_datasets(dataset, test_rate=0.1, valid_rate=0.1)
@@ -178,73 +184,52 @@ if __name__ == '__main__':
     log.info(train)
     log.info(test)
     log.info(validation)
-    log.info('split to train, test, validation OK.')
     log.info('%s %s' % (test.features[0], test.labels[0]))
     log.info('%s %s %s' % (type(test.features), test.features.shape, test.features.dtype))
     log.info('%s %s %s' % (type(test.labels), test.labels.shape, test.labels.dtype))
+    log.info('split to train, test, validation OK.\n')
 
-    # log.info('check samples...')
-    # for i, (features_batch, labels_batch) in enumerate(datasets.test.next_batch(batch_size=10), 1):
-    #     if i > 3:
-    #         break
-    #     for chars, has_space in zip(features_batch, labels_batch):
-    #         chars_v = features_vector.to_vectors(chars)  # vectors of two characters
-    #         feature = np.concatenate(chars_v)  # concated feature
-    #         label = labels_vector.to_vector(has_space)
-    #         log.info('[%s] %s -> %s, %s (len=%s) (len=%s)' % (i, chars, label, feature, len(feature), len(label)))
-    # log.info('check samples OK.')
+    log.info('create tensorflow graph...')
+    n_features = len(train.features[0])  # number of features = 17,380 * 2
+    n_classes = len(train.labels[0])  # number of classes = 2
+    n_hidden1 = 100  # nueron
+    log.info('n_features: %s' % n_features)
+    log.info('n_classes: %s' % n_classes)
+    log.info('n_hidden1: %s' % n_hidden1)
 
-    # @formatter:off
-    # log.info('characters to vector...')  # TODO: convert to vector on loading time?
-    # train_features, train_labels = chars2vector(features=train.features, labels=train.labels)  # character -> vector
-    # valid_features, valid_labels = chars2vector(features=validation.features, labels=validation.labels)  # character -> vector
-    # test_features, test_labels = chars2vector(features=test.features, labels=test.labels)  # character -> vector
-    # log.info('characters to vector OK.')
+    tf.set_random_seed(777)  # for reproducibility
+    learning_rate = 0.1
 
-    # log.info('create tensorflow graph...')
-    # n_features = len(train_features[0])  # number of features = 17,380 * 2
-    # n_classes = len(train_labels[0])  # number of classes = 2
-    # n_hidden1 = 100  # nueron
-    # log.info('n_features: %s' % n_features)
-    # log.info('n_classes: %s' % n_classes)
-    # log.info('n_hidden1: %s' % n_hidden1)
-    #
-    # tf.set_random_seed(777)  # for reproducibility
-    # learning_rate = 0.1
-    #
-    # X = tf.placeholder(tf.float32, [None, n_features])  # two characters
-    # Y = tf.placeholder(tf.float32, [None, n_classes])
-    #
-    # W1 = tf.Variable(tf.random_normal([n_features, n_hidden1]), name='weight1')
-    # b1 = tf.Variable(tf.random_normal([n_hidden1]), name='bias1')
-    # layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
-    #
-    # W2 = tf.Variable(tf.random_normal([n_hidden1, n_classes]), name='weight2')
-    # b2 = tf.Variable(tf.random_normal([n_classes]), name='bias2')
-    # hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
-    #
-    # cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))  # cost/loss function
-    # train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
-    #
-    # predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)  # 0 <= hypothesis <= 1
-    # accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
-    # log.info('create tensorflow graph OK.')
-    #
-    # log.info('learning...')
-    # with tf.Session() as sess:
-    #     sess.run(tf.global_variables_initializer())
-    #
-    #     for step, (features_batch, labels_batch) in enumerate(train.next_batch(batch_size=10), 1):
-    #         features_batch, labels_batch = chars2vector(features=features_batch, labels=labels_batch)  # character -> vector
-    #
-    #         sess.run(train, feed_dict={X: features_batch, Y: labels_batch})
-    #         # if step % 100 == 0:
-    #         log.debug('feature, label: %s -> %s' % (features_batch[0], labels_batch[0]))  # test
-    #         log.info('[%s] cost: %s' % (step, sess.run(cost, feed_dict={X: valid_features, Y: valid_labels})))
-    #         log.info(sess.run(W1))
-    #         log.info(sess.run(W2))
-    #
-    #     # Accuracy report
-    #     h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: test_features, Y: test_labels})
-    # log.info('learning OK.')
-    # log.info("Hypothesis: %s, Correct: %s, Accuracy: %s", (h, c, a))
+    X = tf.placeholder(tf.float32, [None, n_features])  # two characters
+    Y = tf.placeholder(tf.float32, [None, n_classes])
+
+    W1 = tf.Variable(tf.random_normal([n_features, n_hidden1]), name='weight1')
+    b1 = tf.Variable(tf.random_normal([n_hidden1]), name='bias1')
+    layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+
+    W2 = tf.Variable(tf.random_normal([n_hidden1, n_classes]), name='weight2')
+    b2 = tf.Variable(tf.random_normal([n_classes]), name='bias2')
+    hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+
+    cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))  # cost/loss function
+    train_step = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)  # 0 <= hypothesis <= 1
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+    log.info('create tensorflow graph OK.\n')
+
+    log.info('learning...')
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for step, (features_batch, labels_batch) in enumerate(train.next_batch(batch_size=10), 1):
+            sess.run(train_step, feed_dict={X: features_batch, Y: labels_batch})
+            if step % 100 == 0:
+                log.info('[%s] cost: %s' % (step, sess.run(cost, feed_dict={X: validation.features, Y: validation.labels})))
+                # log.info(sess.run(W1))
+                # log.info(sess.run(W2))
+
+        # Accuracy report
+        h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: test.features, Y: test.labels})
+    log.info('learning OK.\n')
+    log.info("Hypothesis: %s, Correct: %s, Accuracy: %s", (h, c, a))
