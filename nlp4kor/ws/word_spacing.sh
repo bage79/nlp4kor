1f97039b1f9e3d09d211eaa69a353f520cdac125
Hyewoong Park <bage@smfcast.com>
2017-06-28 00:22:54 +0900

    ..

--- nlp4kor/ws/word_spacing.sh
+++ nlp4kor/ws/word_spacing.sh
@@ -3,12 +3,12 @@ PROGRAM="word_spacing.py"
 PROJECT_DIR=${HOME}'/workspace/nlp4kor'
 
 if [ "$#" -ne 3 ]; then
-    echo "[Usage] $0 max_sentences left_gram right_gram"
-    echo "[Usage] $0 100 2 2"
+    echo "[Usage] $0 n_train left_gram right_gram"
+    echo "[Usage] $0 1000000 2 2"
     exit
 fi
 
-max_sentences=$1
+n_train=$1
 left_gram=$2
 right_gram=$3
 
@@ -20,5 +20,5 @@ pkill -f ${PROGRAM}
 echo "rm -f logs/${PROGRAM}.*"
 rm -f logs/${PROGRAM}.*
 
-echo "python3 ./${PROGRAM} $max_sentences $left_gram $right_gram >/dev/null 2>&1 &"
-nohup python3 ./${PROGRAM} $max_sentences $left_gram $right_gram >/dev/null 2>&1 &
+echo "python3 ./${PROGRAM} $n_train $left_gram $right_gram >/dev/null 2>&1 &"
+nohup python3 ./${PROGRAM} $n_train $left_gram $right_gram >/dev/null 2>&1 &
