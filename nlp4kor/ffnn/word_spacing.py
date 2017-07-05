a5c9010c39e40aa412888c87ee97cb54565aa865
Hyewoong Park <bage79@gmail.com>
2017-07-06 01:11:59 +0900

    tf.nn.sigmoid_cross_entropy_with_logits + tf.nn.relu

--- nlp4kor/ffnn/word_spacing.py
+++ nlp4kor/ffnn/word_spacing.py
@@ -12,6 +12,7 @@ from bage_utils.dataset import DataSet
 from bage_utils.file_util import FileUtil
 from bage_utils.num_util import NumUtil
 from bage_utils.one_hot_vector import OneHotVector
+from bage_utils.slack_util import SlackUtil
 from bage_utils.watch_util import WatchUtil
 from nlp4kor.config import log, KO_WIKIPEDIA_ORG_DIR, KO_WIKIPEDIA_ORG_CHARACTERS_FILE, \
     KO_WIKIPEDIA_ORG_WORD_SPACING_MODEL_DIR, KO_WIKIPEDIA_ORG_TRAIN_SENTENCES_FILE, KO_WIKIPEDIA_ORG_TEST_SENTENCES_FILE, KO_WIKIPEDIA_ORG_VALID_SENTENCES_FILE, \
@@ -22,8 +23,8 @@ class WordSpacing(object):
     graph_nodes = {}
 
     @classmethod
-    def learning(cls, n_train, n_valid, n_test, batch_size, left_gram, right_gram, model_file, features_vector, labels_vector, n_hidden1=100,
-                 learning_rate=0.01):
+    def learning(cls, total_epoch, n_train, n_valid, n_test, batch_size, left_gram, right_gram, model_file, features_vector, labels_vector, n_hidden1=100,
+                 learning_rate=0.01, early_stop_cost=0.001):
         ngram = left_gram + right_gram
         n_features = len(features_vector) * ngram  # number of features = 17,380 * 4
         n_classes = len(labels_vector) if len(labels_vector) >= 3 else 1  # number of classes = 2 but len=1
@@ -33,10 +34,12 @@ class WordSpacing(object):
         watch = WatchUtil()
 
         train_file = os.path.join(KO_WIKIPEDIA_ORG_DIR, 'datasets', 'word_spacing',
-                                  'ko.wikipedia.org.dataset.left=%d.right=%d.train.gz' % (left_gram, right_gram))
-        valid_file = train_file.replace('.train.', '.valid.')
-        test_file = train_file.replace('.train.', '.test.')
-        if True:  # FIXME: TEST not os.path.exists(train_file) or not os.path.exists(valid_file) or not os.path.exists(test_file):
+                                  'ko.wikipedia.org.dataset.sentences=%s.left=%d.right=%d.train.gz' % (n_train, left_gram, right_gram))
+        valid_file = os.path.join(KO_WIKIPEDIA_ORG_DIR, 'datasets', 'word_spacing',
+                                  'ko.wikipedia.org.dataset.sentences=%s.left=%d.right=%d.test.gz' % (n_valid, left_gram, right_gram))
+        test_file = os.path.join(KO_WIKIPEDIA_ORG_DIR, 'datasets', 'word_spacing',
+                                 'ko.wikipedia.org.dataset.sentences=%s.left=%d.right=%d.valid.gz' % (n_test, left_gram, right_gram))
+        if not os.path.exists(train_file) or not os.path.exists(valid_file) or not os.path.exists(test_file):
             dataset_dir = os.path.dirname(train_file)
             if not os.path.exists(dataset_dir):
                 os.makedirs(dataset_dir)
@@ -44,68 +47,57 @@ class WordSpacing(object):
             watch.start('create dataset')
             log.info('create dataset...')
 
-            for name, data_file, dataset_file, to_one_hot_vector in (('valid', KO_WIKIPEDIA_ORG_VALID_SENTENCES_FILE, valid_file, False),
-                                                                     ('test', KO_WIKIPEDIA_ORG_TEST_SENTENCES_FILE, test_file, False),
-                                                                     ('train', KO_WIKIPEDIA_ORG_TRAIN_SENTENCES_FILE, train_file, False),
-                                                                     ):
-                max_train_sentences = FileUtil.count_lines(data_file, gzip_format=True)
-                features, labels = [], []
-                check_interval = 10000
-                log.info('total: %s' % NumUtil.comma_str(max_train_sentences))
+            data_files = (('train', KO_WIKIPEDIA_ORG_TRAIN_SENTENCES_FILE, n_train, train_file, False),
+                          ('valid', KO_WIKIPEDIA_ORG_VALID_SENTENCES_FILE, n_valid, valid_file, False),
+                          ('test', KO_WIKIPEDIA_ORG_TEST_SENTENCES_FILE, n_test, test_file, False))
 
+            for name, data_file, total, dataset_file, to_one_hot_vector in data_files:
+                check_interval = max(1, min(10000, batch_size // 10))
+                log.info('check_interval: %s' % check_interval)
+                log.info('%s %s total: %s' % (name, os.path.basename(data_file), NumUtil.comma_str(total)))
+
+                features, labels = [], []
                 with gzip.open(data_file, 'rt') as f:
                     for i, line in enumerate(f, 1):
-                        # if name == 'train' and max_train_sentences < i:
-                        #     break
+                        if total < i:
+                            break
 
                         if i % check_interval == 0:
-                            time.sleep(0.1)  # prevent cpu overload
-                            log.info('create dataset... %.1f%% readed. data len: %s' % (i / max_train_sentences * 100, NumUtil.comma_str(len(features))))
+                            time.sleep(0.01)  # prevent cpu overload
+                            percent = i / total * 100
+                            log.info('create dataset... %.1f%% readed. data len: %s. %s' % (percent, NumUtil.comma_str(len(features)), data_file))
 
                         _f, _l = WordSpacing.sentence2features_labels(line.strip(), left_gram=left_gram, right_gram=right_gram)
                         features.extend(_f)
                         labels.extend(_l)
 
-                    dataset = DataSet(features=features, labels=labels, features_vector=features_vector, labels_vector=labels_vector, name=name)
-                    log.info('dataset save... %s' % dataset_file)
-                    dataset.save(dataset_file, gzip_format=True, verbose=True)
-                    log.info('dataset save OK. %s' % dataset_file)
-                    log.info('dataset: %s' % dataset)
+                dataset = DataSet(features=features, labels=labels, features_vector=features_vector, labels_vector=labels_vector, name=name)
+                log.info('dataset save... %s' % dataset_file)
+                dataset.save(dataset_file, gzip_format=True, verbose=True)
+                log.info('dataset save OK. %s' % dataset_file)
+                log.info('dataset: %s' % dataset)
 
-            log.info('create dataset OK.\n')
+            log.info('create dataset OK.')
+            log.info('')
             watch.stop('create dataset')
 
         watch.start('dataset load')
         log.info('dataset load...')
-        train = DataSet.load(train_file, gzip_format=True, max_len=n_train, verbose=True)
+        train = DataSet.load(train_file, gzip_format=True, verbose=True)
 
-        valid = DataSet.load(valid_file, gzip_format=True, max_len=n_valid, verbose=True)
+        if n_train >= int('100,000'.replace(',', '')):
+            valid = DataSet.load(valid_file, gzip_format=True, verbose=True)
+        else:
+            valid = DataSet.load(train_file, gzip_format=True, verbose=True)
         log.info('valid.convert_to_one_hot_vector()...')
         valid = valid.convert_to_one_hot_vector(verbose=True)
         log.info('valid.convert_to_one_hot_vector() OK.')
 
-        test = DataSet.load(test_file, gzip_format=True, max_len=n_test, verbose=True)
-        log.info('test.convert_to_one_hot_vector()...')
-        test = test.convert_to_one_hot_vector(verbose=True)
-        log.info('test.convert_to_one_hot_vector() OK.')
-
-        log.info(train)
-        log.info(valid)
-        log.info(test)
-        log.info('dataset load OK.\n')
+        log.info('train dataset: %s' % train)
+        log.info('valid dataset: %s' % valid)
+        log.info('dataset load OK.')
+        log.info('')
         watch.stop('dataset load')
-
-        # log.info('check samples...')
-        # for i, (features_batch, labels_batch) in enumerate(train.next_batch(batch_size=5, to_one_hot_vector=True), 1):
-        #     if i > 2:
-        #         break
-        #     for a, b in zip(features_batch, labels_batch):
-        #         feature, label = a, b
-        #         _feature = feature.reshape((ngram, len(features_vector)))
-        #         chars = ''.join(features_vector.to_values(_feature))
-        #         has_space = np.argmax(label)
-        #         log.info('[%s] %s -> %s, %s (len=%s) %s (len=%s)' % (i, chars, has_space, feature, len(feature), label, len(label)))
-        # log.info('check samples OK.\n')
 
         graph = WordSpacing.build_FFNN(n_features, n_classes, n_hidden1, learning_rate, watch)
 
@@ -113,24 +105,30 @@ class WordSpacing(object):
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
+            check_interval = 10  # max(1, min(1000, n_train // 10))
+            nth_train, nth_input, total_input = 0, 0, total_epoch * train.size
 
-            n_input = 0
-            log.info('total: %s' % NumUtil.comma_str(train.size))
             log.info('learn...')
+            log.info('total: %s' % NumUtil.comma_str(train.size))
             watch.start('learn')
-            for step, (features_batch, labels_batch) in enumerate(train.next_batch(batch_size=batch_size), 1):
-                n_input += batch_size
-                sess.run(train_step, feed_dict={X: features_batch, Y: labels_batch})
-                log.info('[%s][%.1f%%] valid cost: %.4f' % (NumUtil.comma_str(n_input), n_input / train.size * 100,
-                                                            sess.run(cost, feed_dict={X: valid.features, Y: valid.labels})))
+            valid_cost = sys.float_info.max
+            for epoch in range(1, total_epoch + 1):
+                if valid_cost < early_stop_cost:
+                    break
+                for step, (features_batch, labels_batch) in enumerate(train.next_batch(batch_size=batch_size), 1):
+                    if valid_cost < early_stop_cost:
+                        log.info('valid_cost: %s, early_stop_cost: %s, early stopped.' % (valid_cost, early_stop_cost))
+                        break
+                    nth_train += 1
+                    nth_input += features_batch.shape[0]
+                    sess.run(train_step, feed_dict={X: features_batch, Y: labels_batch})
+
+                    # if step % check_interval == 1:
+                    percent = nth_input / total_input * 100
+                    valid_cost = sess.run(cost, feed_dict={X: valid.features, Y: valid.labels})
+                    log.info('[epoch=%s][%.1f%%] %s cost: %.4f' % (epoch, percent, valid.name, valid_cost))
             watch.stop('learn')
             log.info('learn OK.\n')
-
-            log.info('evaluate...')
-            watch.start('evaluate...')
-            _correct, _accuracy = sess.run([predicted, accuracy], feed_dict={X: test.features, Y: test.labels})  # Accuracy report
-            watch.stop('evaluate...')
-            log.info('evaluate OK.')
 
             log.info('model save... %s' % model_file)
             watch.start('model save...')
@@ -143,10 +141,8 @@ class WordSpacing(object):
             log.info('model save OK. %s' % model_file)
 
         log.info('\n')
+        log.info('batch_size: %s' % batch_size)
         log.info(watch.summary())
-        # log.info('hypothesis: %s %s' % (_hypothesis.shape, _hypothesis))
-        # log.info('correct: %s %s' % (_correct.shape, _correct))
-        log.info('accuracy: %s %s' % (_accuracy.shape, _accuracy))
         log.info('\n')
 
     @classmethod
@@ -207,26 +203,28 @@ class WordSpacing(object):
 
             W1 = tf.Variable(tf.random_normal([n_features, n_hidden1]), name='W1')
             b1 = tf.Variable(tf.random_normal([n_hidden1]), name='b1')
-            layer1 = tf.sigmoid(tf.matmul(X, W1) + b1, name='layer1')
+            layer1 = tf.nn.relu(tf.matmul(X, W1) + b1, name='layer1')
 
             W2 = tf.Variable(tf.random_normal([n_hidden1, n_hidden2]), name='W2')
             b2 = tf.Variable(tf.random_normal([n_hidden2]), name='b2')
-            layer2 = tf.sigmoid(tf.matmul(layer1, W2) + b2, name='layer2')
+            layer2 = tf.nn.relu(tf.matmul(layer1, W2) + b2, name='layer2')
 
             W3 = tf.Variable(tf.random_normal([n_hidden2, n_hidden3]), name='W3')
             b3 = tf.Variable(tf.random_normal([n_hidden3]), name='b3')
-            layer3 = tf.sigmoid(tf.matmul(layer2, W3) + b3, name='layer3')
+            layer3 = tf.nn.relu(tf.matmul(layer2, W3) + b3, name='layer3')
 
             W4 = tf.Variable(tf.random_normal([n_hidden3, n_classes]), name='W4')
             b4 = tf.Variable(tf.random_normal([n_classes]), name='b4')
-            hypothesis = tf.sigmoid(tf.matmul(layer3, W4) + b4, name='hypothesis')
+            y_hat = tf.add(tf.matmul(layer3, W4), b4, name='y_hat')
 
-            cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis), name='cost')  # cost/loss function
+            # cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis), name='cost')  # cost/loss function
+            cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_hat, labels=Y), name='cost')
 
             train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(
                 cost)  # Very Very good!! sentences=10000 + layer=4, 10분, accuracy 0.9294, cost: 0.1839
 
-            predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32, name='predicted')  # 0 <= hypothesis <= 1
+            predicted = tf.cast(y_hat > 0.5, dtype=tf.float32, name='predicted')  # 0 <= hypothesis <= 1
+
             accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32), name='accuracy')
             watch.stop('create tensorflow graph')
             log.info('create tensorflow graph OK.\n')
@@ -295,18 +293,21 @@ if __name__ == '__main__':
         batch_size = 1000  # mini batch size
         log.info('batch_size: %s' % batch_size)
 
+        total_epoch = min(100, 1000000 // n_train)  # 1 ~ 100
         features_vector = OneHotVector(DataFileUtil.read_list(characters_file))
         labels_vector = OneHotVector([0, 1])  # 붙여쓰기=0, 띄어쓰기=1
         n_features = len(features_vector) * ngram  # number of features = 17,380 * 4
         n_classes = len(labels_vector) if len(labels_vector) >= 3 else 1  # number of classes = 2 but len=1
         n_hidden1 = 100
-        learning_rate = 0.01  # 0.1 ~ 0.001
+        learning_rate = min(0.1, 0.001 * total_epoch)  # 0.1 ~ 0.001
+        early_stop_cost = 0.0001
         log.info('features_vector: %s' % features_vector)
         log.info('labels_vector: %s' % labels_vector)
         log.info('n_features: %s' % n_features)
         log.info('n_classes: %s' % n_classes)
         log.info('n_hidden1: %s' % n_hidden1)
         log.info('learning_rate: %s' % learning_rate)
+        log.info('early_stop_cost: %s' % early_stop_cost)
 
         # log.info('sample testing...')
         # test_set = ['예쁜 운동화', '즐거운 동화', '삼풍동 화재']
@@ -318,16 +319,26 @@ if __name__ == '__main__':
         # log.info('sample testing OK.\n')
 
         if not os.path.exists(model_file + '.index') or not os.path.exists(model_file + '.meta'):
-            WordSpacing.learning(n_test, n_valid, n_test, batch_size, left_gram, right_gram, model_file, features_vector, labels_vector, n_hidden1=n_hidden1,
-                                 learning_rate=learning_rate)
+            if n_train >= int('100,000'.replace(',', '')):
+                SlackUtil.send_message('%s start (max_sentences=%s, left_gram=%s, right_gram=%.1f)' % (sys.argv[0], n_train, left_gram, right_gram))
+            WordSpacing.learning(total_epoch, n_test, n_valid, n_test, batch_size, left_gram, right_gram, model_file, features_vector, labels_vector,
+                                 n_hidden1=n_hidden1,
+                                 learning_rate=learning_rate, early_stop_cost=early_stop_cost)
+            if n_train >= int('100,000'.replace(',', '')):
+                SlackUtil.send_message('%s end (max_sentences=%s, left_gram=%s, right_gram=%.1f)' % (sys.argv[0], n_train, left_gram, right_gram))
 
         log.info('chek result...')
         watch = WatchUtil()
         watch.start('read sentences')
 
-        sentences = ['아버지가 방에 들어 가신다.', '가는 말이 고와야 오는 말이 곱다.']
+        sentences = []  # '아버지가 방에 들어 가신다.', '가는 말이 고와야 오는 말이 곱다.']
         max_test_sentences = 100
-        with gzip.open(test_sentences_file, 'rt') as f:
+        if n_train >= int('100,000'.replace(',', '')):
+            sentences_file = test_sentences_file
+        else:
+            sentences_file = train_sentences_file
+
+        with gzip.open(sentences_file, 'rt') as f:
             for i, line in enumerate(f, 1):
                 if len(sentences) >= max_test_sentences:
                     break
@@ -353,7 +364,7 @@ if __name__ == '__main__':
                 for i, s in enumerate(sentences):
                     log.info('')
                     log.info('[%s] in : "%s"' % (i, s))
-                    _features, _labels = WordSpacing.sentence2features_labels(s, left_gram, right_gram)  # TODO: with test set
+                    _features, _labels = WordSpacing.sentence2features_labels(s, left_gram, right_gram)
                     dataset = DataSet(features=_features, labels=_labels, features_vector=features_vector, labels_vector=labels_vector)
                     dataset.convert_to_one_hot_vector()
                     if len(dataset) > 0:
