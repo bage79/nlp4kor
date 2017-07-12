dbffb04171338b542a7d69bb8862db5139f1b6af
Hyewoong Park <bage79@gmail.com>
2017-07-12 09:37:54 +0900

    ..

--- bage_utils/plot_util.py
+++ bage_utils/plot_util.py
@@ -1,7 +1,7 @@
 class PlotUtil(object):
     @staticmethod
-    def pixel2inch(pixels):
+    def pixel2inch(pixels, dpi=96):
         if isinstance(pixels, tuple) or isinstance(pixels, list):
-            return pixels[0] / 96, pixels[1] / 96
+            return pixels[0] / dpi, pixels[1] / dpi
         elif isinstance(pixels, int) or isinstance(pixels, float):
-            return pixels / 96
+            return pixels / dpi
