54892384ace8c594c5ec94c98cda844de3e4913c
Hyewoong Park <bage@smfcast.com>
2017-09-01 23:12:37 +0900

    recover to old version.

--- nlp4kor/ffnn/word_spacing.py
+++ nlp4kor/ffnn/word_spacing.py
@@ -1,8 +1,9 @@
 import gzip
 import math
 import os
+import sys
+import traceback
 
-import editdistance
 import numpy as np
 import tensorflow as tf
 
@@ -10,11 +11,10 @@ from bage_utils.base_util import is_my_pc
 from bage_utils.datafile_util import DataFileUtil
 from bage_utils.dataset import DataSet
 from bage_utils.datasets import DataSets
-from bage_utils.file_util import FileUtil
 from bage_utils.num_util import NumUtil
 from bage_utils.char_one_hot_vector import CharOneHotVector
 from bage_utils.watch_util import WatchUtil
-from nlp4kor.config import log, WIKIPEDIA_DATA_DIR, WIKIPEDIA_SENTENCES_FILE, WIKIPEDIA_DIR
+from nlp4kor.config import log, WIKIPEDIA_DATA_DIR, WIKIPEDIA_SENTENCES_FILE
 
 
 class WordSpacing(object):
@@ -59,9 +59,9 @@ class WordSpacing(object):
         return ''.join(left)
 
     @classmethod
-    def build_FFNN(cls, n_features, n_classes, n_hidden1, learning_rate, watch=WatchUtil(), layers=1):
+    def build_FFNN(cls, n_features, n_classes, n_hidden1, learning_rate, watch=WatchUtil(), layers=4):
         log.info('\nbuild_FFNN(layers=%s)' % layers)
-        if layers == 1:
+        if layers == 2:
             return cls.__build_FFNN_layers2(n_features, n_classes, n_hidden1, learning_rate, watch=watch)
         else:
             return cls.__build_FFNN_layers4(n_features, n_classes, n_hidden1, learning_rate, watch=watch)
@@ -136,7 +136,8 @@ class WordSpacing(object):
             cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis), name='cost')  # cost/loss function
 
             # train_step = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)  # Too bad. sentences=10000 + layer=2, 20분, Accuracy: 0.689373, cost: 0.8719
-            train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)  # Very good!! sentences=10000 + layer=2, 10분, accuracy 0.9194, cost: 0.2139
+            train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(
+                cost)  # Very good!! sentences=10000 + layer=2, 10분, accuracy 0.9194, cost: 0.2139
 
             predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32, name='predicted')  # 0 <= hypothesis <= 1
             accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32), name='accuracy')
@@ -156,7 +157,9 @@ class WordSpacing(object):
         log.info('load characters list OK. len: %s\n' % NumUtil.comma_str(len(features_vector)))
         watch = WatchUtil()
 
-        train_file = os.path.join(WIKIPEDIA_DIR, '_datasets', 'ko.wikipedia.org.dataset.%d.left=%d.right=%d.train.gz' % (max_sentences, left_gram, right_gram)); validation_file = train_file.replace('.train.', '.valid.')
+        train_file = os.path.join(WIKIPEDIA_DATA_DIR, 'datasets',
+                                  'ko.wikipedia.org.dataset.%d.left=%d.right=%d.train.gz' % (max_sentences, left_gram, right_gram))
+        validation_file = train_file.replace('.train.', '.validation.')
         test_file = train_file.replace('.train.', '.test.')
         if not os.path.exists(train_file) or not os.path.exists(validation_file) or not os.path.exists(test_file):
             watch.start('create dataset')
@@ -180,14 +183,14 @@ class WordSpacing(object):
             dataset = DataSet(features=features, labels=labels, features_vector=features_vector, labels_vector=labels_vector)
             log.info('dataset: %s' % dataset)
 
-            log.info('split to train, test, valid...')
+            log.info('split to train, test, validation...')
             datasets = DataSets.to_datasets(dataset, test_rate=0.1, valid_rate=0.1, test_max=10000, valid_max=1000, shuffle=True)
-            train, test, valid = datasets.train, datasets.test, datasets.valid
+            train, test, validation = datasets.train, datasets.test, datasets.validation
             log.info(train)
             log.info(test)
-            log.info(valid)
+            log.info(validation)
             # log.info('%s %s' % (test.features[0], test.labels[0]))
-            log.info('split to train, test, valid OK.\n')
+            log.info('split to train, test, validation OK.\n')
 
             log.info('dataset save... %s' % train_file)
             train.save(train_file, verbose=True)  # save as text # TODO: counter 형식으로 압축해서 저장할까?
@@ -195,8 +198,8 @@ class WordSpacing(object):
             log.info('create dataset OK.\n')
 
             log.info('dataset save... %s' % validation_file)
-            valid = valid.convert_to_one_hot_vector(verbose=True)  # save as vector
-            valid.save(validation_file, verbose=True)
+            validation = validation.convert_to_one_hot_vector(verbose=True)  # save as vector
+            validation.save(validation_file, verbose=True)
             log.info('dataset save OK.\n')
 
             log.info('dataset save... %s' % test_file)
@@ -208,10 +211,10 @@ class WordSpacing(object):
             watch.start('dataset load')
             log.info('dataset load...')
             train = DataSet.load(train_file, verbose=True)
-            valid = DataSet.load(validation_file, verbose=True)
+            validation = DataSet.load(validation_file, verbose=True)
             test = DataSet.load(test_file, verbose=True)
             log.info(train)
-            log.info(valid)
+            log.info(validation)
             log.info(test)
             log.info('dataset load OK.\n')
             watch.stop('dataset load')
@@ -243,8 +246,8 @@ class WordSpacing(object):
             for step, (features_batch, labels_batch) in enumerate(train.next_batch(batch_size=batch_size), 1):
                 n_input += batch_size
                 sess.run(train_step, feed_dict={X: features_batch, Y: labels_batch})
-                log.info('[%s][%.1f%%] valid cost: %.4f' % (NumUtil.comma_str(n_input), n_input / train.size * 100,
-                                                                 sess.run(cost, feed_dict={X: valid.features, Y: valid.labels})))
+                log.info('[%s][%.1f%%] validation cost: %.4f' % (NumUtil.comma_str(n_input), n_input / train.size * 100,
+                                                                 sess.run(cost, feed_dict={X: validation.features, Y: validation.labels})))
             watch.stop('learn')
             log.info('learn OK.\n')
 
@@ -272,12 +275,53 @@ class WordSpacing(object):
         log.info('\n')
 
     @classmethod
-    def sim_two_sentence(cls, original, generated):
-        total = len(original.replace(' ', '')) - 1
-        return 1 - editdistance.eval(original, generated) / total
+    def levenshteinDistance(cls, s1, s2):
+        if len(s1) > len(s2):
+            s1, s2 = s2, s1
+
+        distances = range(len(s1) + 1)
+        for i2, c2 in enumerate(s2):
+            distances_ = [i2 + 1]
+            for i1, c1 in enumerate(s1):
+                if c1 == c2:
+                    distances_.append(distances[i1])
+                else:
+                    distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+            distances = distances_
+        return distances[-1]
+
+    @classmethod
+    def sim_two_sentence(cls, original, generated, left_gram=2, right_gram=2):
+        _, labels1 = WordSpacing.sentence2features_labels(original, left_gram=left_gram, right_gram=right_gram)
+        _, labels2 = WordSpacing.sentence2features_labels(generated, left_gram=left_gram, right_gram=right_gram)
+        incorrect = 0
+        for idx, l in enumerate(labels1):
+            if l == 1 and labels2[idx] != 1:
+                incorrect += 1
+
+        total_spaces = labels1.count(1)  # 정답에 있는 공백 개수
+        correct = total_spaces - incorrect  # 정답에 있는 공백과 같은 곳에 공백이 있는지
+
+        if total_spaces == 0:
+            sim = 1
+        else:
+            sim = correct / total_spaces
+        return sim, correct, total_spaces
 
 
 if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        max_sentences = int(sys.argv[1])
+    else:
+        max_sentences = int('1,000,000'.replace(',', '')) if is_my_pc() else int('1,000,000'.replace(',', ''))  # run 100 or 1M data (학습: 17시간 소요)
+    # max_sentences = 100 if is_my_pc() else FileUtil.count_lines(sentences_file, gzip_format=True) # run 100 or full data (학습시간: 5일 소요)
+    layers = 4
+    model_file = os.path.join(WIKIPEDIA_DATA_DIR, 'models',
+                              'word_spacing_model.sentences=%s.layers=%s/model' % (max_sentences, layers))  # .%s' % max_sentences
+    log.info('max_sentences: %s' % max_sentences)
+    log.info('layers: %s' % layers)
+    log.info('model_file: %s' % model_file)
+
     sentences_file = WIKIPEDIA_SENTENCES_FILE
     log.info('sentences_file: %s' % sentences_file)
 
@@ -300,52 +344,37 @@ if __name__ == '__main__':
     log.info('n_features: %s' % n_features)
     log.info('n_classes: %s' % n_classes)
 
-    layers = 4
     n_hidden1 = 100
     learning_rate = 0.01  # 0.1 ~ 0.001
-    # max_sentences = 100 if is_my_pc() else FileUtil.count_lines(sentences_file, gzip_format=True) # run 100 or full data
-    max_sentences = 10000 if is_my_pc() else int('1,000,000'.replace(',', ''))  # run 100 or 1M data
-    model_file = os.path.join(WIKIPEDIA_DATA_DIR, 'models',
-                              'word_spacing_model.sentences=%s.layers=%s/' % (max_sentences, layers))  # .%s' % max_sentences
-    # model_file = os.path.join(WIKIPEDIA_DATA_DIR, 'models', 'word_spacing_model/')  # .%s' % max_sentences
-    log.info('layers: %s' % layers)
     log.info('n_hidden1: %s' % n_hidden1)
     log.info('learning_rate: %s' % learning_rate)
-    log.info('max_sentences: %s' % max_sentences)
-    log.info('model_file: %s' % model_file)
-
-    log.info('sample learning...')
-    train_set = ['예쁜 운동화']
-    features, labels = [], []
-    for s in train_set:
-        features, labels = WordSpacing.sentence2features_labels(s, left_gram=left_gram, right_gram=right_gram)
-        log.info('%s %s' % (features, labels))
-    log.info('sample learning OK.\n')
 
     log.info('sample testing...')
     test_set = ['예쁜 운동화', '즐거운 동화', '삼풍동 화재']
     for s in test_set:
-        features, labels = WordSpacing.sentence2features_labels(s, left_gram=left_gram, right_gram=right_gram)  # high accuracy # do not use
-        log.info('')
+        features, labels = WordSpacing.sentence2features_labels(s, left_gram=left_gram, right_gram=right_gram)
         log.info('%s -> %s' % (features, labels))
         log.info('in : "%s"' % s)
-        log.info('out: "%s"' % WordSpacing.spacing(s, labels))
+        log.info('out: "%s"' % WordSpacing.spacing(s.replace(' ', ''), labels))
     log.info('sample testing OK.\n')
 
-    if not os.path.exists(model_file):
+    if not os.path.exists(model_file + '.index') or not os.path.exists(model_file + '.meta'):
         WordSpacing.learning(sentences_file, batch_size, left_gram, right_gram, model_file, features_vector, labels_vector, n_hidden1=n_hidden1,
                              max_sentences=max_sentences, learning_rate=learning_rate, layers=layers)
 
+    watch = WatchUtil()
+    watch.start('check')
     log.info('chek result...')
-    # sentences = ['아버지가 방에 들어 가신다.']
-    sentences = []
-    max_test_sentences = 10
+    sentences = ['아버지가 방에 들어 가신다.', '가는 말이 고와야 오는 말이 곱다.']
+    max_test_sentences = 100
     with gzip.open(sentences_file, 'rt') as f:
-        for i, line in enumerate(f):
-            if i + 1 > max_test_sentences:
+        for line in f:
+            if len(sentences) >= max_test_sentences:
                 break
             sentences.append(line.strip())
+    log.info('len(sentences): %s' % NumUtil.comma_str(len(sentences)))
 
+    accuracies, sims = [], []
     with tf.Session() as sess:
         graph = WordSpacing.build_FFNN(n_features, n_classes, n_hidden1, learning_rate, layers=layers)
         X, Y, predicted, accuracy = graph['X'], graph['Y'], graph['predicted'], graph['accuracy']
@@ -353,18 +382,30 @@ if __name__ == '__main__':
         saver = tf.train.Saver()
         try:
             restored = saver.restore(sess, model_file)
-            for s in sentences:
+        except:
+            log.error('restore failed. model_file: %s' % model_file)
+        try:
+            for i, s in enumerate(sentences):
+                log.info('')
+                log.info('[%s] in : "%s"' % (i, s))
                 features, labels = WordSpacing.sentence2features_labels(s, left_gram, right_gram)
                 dataset = DataSet(features=features, labels=labels, features_vector=features_vector, labels_vector=labels_vector)
                 dataset.convert_to_one_hot_vector()
-                _predicted, _accuracy = sess.run([predicted, accuracy], feed_dict={X: dataset.features, Y: dataset.labels})  # Accuracy report
-                generated_sentence = WordSpacing.spacing(s.replace(' ', ''), _predicted)
-                distance = WordSpacing.sim_two_sentence(s, generated_sentence)
-                log.info('')
-                log.info('in : "%s"' % s)
-                log.info(
-                    'out: "%s" (accuracy: %s%%, distance: %s%%)' % (generated_sentence, NumUtil.comma_str(_accuracy * 100), NumUtil.comma_str(distance * 100)))
+                if len(dataset) > 0:
+                    _predicted, _accuracy = sess.run([predicted, accuracy], feed_dict={X: dataset.features, Y: dataset.labels})  # Accuracy report
+
+                    generated_sentence = WordSpacing.spacing(s.replace(' ', ''), _predicted)
+                    sim, correct, total = WordSpacing.sim_two_sentence(s, generated_sentence, left_gram=left_gram, right_gram=right_gram)
+
+                    accuracies.append(_accuracy)
+                    sims.append(sim)
+
+                    log.info('[%s] out: "%s" (accuracy: %.1f%%, sim: %.1f%%=%s/%s)' % (i, generated_sentence, _accuracy * 100, sim * 100, correct, total))
         except:
-            log.error('restore failed. model_file: %s' % model_file)
+            log.error(traceback.format_exc())
 
     log.info('chek result OK.')
+    # noinspection PyStringFormat
+    log.info('mean(accuracy): %.2f%%, mean(sim): %.2f%%' % (np.mean(accuracies) * 100, np.mean(sims) * 100))
+    log.info('secs/sentence: %.4f' % (watch.elapsed('check') / len(sentences)))
+    log.info(watch.summary())
