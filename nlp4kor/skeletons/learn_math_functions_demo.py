f9283ce02920103c60c81468aa6020a5fcb1e4ce
Hyewoong Park <bage79@gmail.com>
2017-10-08 15:48:15 +0900

    learn math function (add, average, multiply)

--- nlp4kor/skeletons/learn_math_functions_demo.py
+++ nlp4kor/skeletons/learn_math_functions_demo.py
@@ -2,7 +2,6 @@ import math
 import os
 import time
 import traceback
-import sys
 
 import numpy as np
 import tensorflow as tf
@@ -46,22 +45,22 @@ def build_graph(scope_name, n_features, n_hiddens, n_classes, learning_rate, opt
     cost = tf.reduce_mean(tf.square(y - y_hat), name='cost')
     train_step = optimizer(learning_rate=learning_rate, name='optimizer').minimize(cost, name='train_step')
 
-    rsme = tf.sqrt(cost, name='rsme')
+    rmse = tf.sqrt(cost, name='rmse')
     with tf.name_scope(scope_name):
         cost_ = tf.summary.scalar(tensor=cost, name='cost')
         summary = tf.summary.merge([cost_])
-    return x, y, y_hat, cost, rsme, train_step, summary
+    return x, y, y_hat, cost, rmse, train_step, summary
 
 
 if __name__ == '__main__':
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # ignore tensorflow warnings
     tf.logging.set_verbosity(tf.logging.ERROR)  # ignore tensorflow info
 
-    func = multiply
+    func = multiply  # TODO: 다른 데이터 생성 함수로 교체해 볼것 add, average
     n_features = 2  # x1, x2
     n_classes = 1  # y
     digits = list(range(-99, 100, 1))
-    n_train, n_test = 4000, 100  # 1% of 200 * 200
+    n_train, n_test = 4000, 100  # 10% of 200 * 200
 
     x_data = np.random.choice(digits, (n_train + n_test, n_features), replace=True)
     y_data = func(x_data)
@@ -84,31 +83,32 @@ if __name__ == '__main__':
 
     # default values
     optimizer = tf.train.AdamOptimizer
-    activation = tf.nn.sigmoid
+    activation = tf.sigmoid
     weights_initializer = tf.random_normal_initializer
     n_hiddens = 10  # in one layer
     learning_rate = 0.1
     train_time = 1  # secs
 
-    # good values
-    # TODO: 직접 수정해서 최적화해 보세요.
-    # activation = ?
-    # weights_initializer = ?
-    # n_hiddens = ?
-    # learning_rate = ?
-    train_time = 10 * 60
+    # # good values
+    # # TODO: 0. (cost: 2500-3000)
+    activation = tf.nn.relu  # TODO: 1. 학습 가능 여부 (cost: 1700)
+    weights_initializer = tf.truncated_normal_initializer  # TODO: 2. 좀더 안정된 분포 (600-700)(min epoch==total epoch)
+    n_hiddens = 1000  # TODO: 4. 모델 용량 증가 (cost: 500-700) cost 변화 없음. min epoch < total epoch 이므로 learning_rate를 줄여야 함.
+    learning_rate = 0.001  # TODO: 5. 정확도 증가 (cost: 1600) cost 증가했으나 min epoch==total epoch 이므로, cost가 더 줄어들 수 있음 확인. tensorboard 확인
+    train_time = 10 * 60  # TODO: 6. 끝까지 학습  (cost: 4-5) (6분)
+    # # # TODO: 7. 좀더 줄이려면 어떻게 해야 할까요? (decay)
 
     log.info('%s -> %s -> %s -> %s -> %s' % (x_train.shape[1], n_hiddens, activation.__name__, n_hiddens, 1))
     log.info('weights_initializer: %s' % weights_initializer.__name__)
     log.info('learning_rate: %.4f' % learning_rate)
     log.info('train_time: %s' % train_time)
 
-    how_many_trains = 3 if train_time <= 1 else 1  # 1초 실행하는 경우 3번 실험, 그 외에는 1번 실험.
+    how_many_trains = 3 if train_time < 10 else 1
     log.info('how_many_trains: %s' % how_many_trains)
     for _ in range(how_many_trains):
         time.sleep(1)
         tf.reset_default_graph()  # Clears the default graph stack and resets the global default graph.
-        tf.set_random_seed(7942)
+        tf.set_random_seed(7942)  # TODO: 3. 결과를 규칙적으로 만들자. (cost: 600-700)
 
         scope_name = '%s.%s' % (func.__name__, DateUtil.current_yyyymmdd_hhmmss())
         x, y, y_hat, cost, rsme, train_step, summary = build_graph(scope_name, n_features, n_hiddens, n_classes, learning_rate, activation=activation, weights_initializer=weights_initializer,
@@ -119,9 +119,9 @@ if __name__ == '__main__':
             model_file_saved = False
             model_file = os.path.join(MODELS_DIR, '%s_%s/model' % (os.path.basename(__file__.replace('.py', '')), func.__name__))
             model_dir = os.path.dirname(model_file)
-            log.info('model_file: %s' % model_file)
+            # log.info('model_file: %s' % model_file)
             if not os.path.exists(model_dir):
-                log.info('model_dir: %s' % model_dir)
+                # log.info('model_dir: %s' % model_dir)
                 os.makedirs(model_dir)
 
             config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
@@ -132,8 +132,8 @@ if __name__ == '__main__':
                 train_writer = tf.summary.FileWriter(TENSORBOARD_LOG_DIR + '/train', sess.graph)
                 valid_writer = tf.summary.FileWriter(TENSORBOARD_LOG_DIR + '/valid', sess.graph)
 
-                max_cost = sys.float_info.max
-                best_epoch, best_cost = 0, sys.float_info.max
+                max_cost = 1e10
+                best_epoch, best_cost = 0, 1e10
                 watch.start('train')
 
                 running, epoch = True, 0
@@ -154,17 +154,15 @@ if __name__ == '__main__':
                         valid_rsme, valid_summary = sess.run([rsme, summary], feed_dict={x: x_test, y: y_test})
                         valid_writer.add_summary(valid_summary, global_step=epoch)
                         valid_writer.flush()
-                        if valid_rsme < best_cost:  # cost가 작을 때의 모델만...
+                        if valid_rsme < best_cost:
                             best_cost = valid_rsme
                             best_epoch = epoch
-                            saver.save(sess, model_file)  # 저장하자.
+                            saver.save(sess, model_file)
                             model_file_saved = True
-                            if epoch % 10 == 0:
-                                log.info('[epoch: %s] rsme (train/valid): %.1f / %.1f model saved' % (epoch, train_rsme, valid_rsme))
+                            log.info('[epoch: %s] rsme (train/valid): %.1f / %.1f model saved' % (epoch, train_rsme, valid_rsme))
                         else:
-                            if epoch % 10 == 0:
-                                log.info('[epoch: %s] rsme (train/valid): %.1f / %.1f' % (epoch, train_rsme, valid_rsme))
-                        if valid_rsme < early_stop_cost or valid_rsme > max_cost or math.isnan(valid_rsme):  # cost가 너무 크거나, nan이거나 매우 작으면 학습 종료.
+                            log.info('[epoch: %s] rsme (train/valid): %.1f / %.1f' % (epoch, train_rsme, valid_rsme))
+                        if valid_rsme < early_stop_cost or valid_rsme > max_cost or math.isnan(valid_rsme):
                             running = False
                             break
                 watch.stop('train')
@@ -181,7 +179,8 @@ if __name__ == '__main__':
                         func.__name__, test_rsme, NumUtil.comma_str(best_epoch), NumUtil.comma_str(epoch), activation.__name__,
                         n_hiddens, learning_rate, weights_initializer.__name__))
 
-                    for i in range(min(5, _y_hat.shape[0])):  # 최대 5개까지만 출력해서 확인
+                    # _y_hat = np.round(_y_hat)
+                    for i in range(min(5, _y_hat.shape[0])):
                         log.info('%s\t->\t%.1f\t(label: %d)' % (x_test[i], _y_hat[i], y_test[i]))
                     watch.stop('test')
                     log.info('--------TEST----------')
