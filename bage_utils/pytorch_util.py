eb4cc4b55fa1a4a6e4534c0195aadb3af8e7dbf6
Hyewoong Park <bage79@gmail.com>
2017-11-16 15:53:10 +0900

    util for pytorch

---
+++ bage_utils/pytorch_util.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+class PytorchUtil(object):
+    @staticmethod
+    def equal_distribution(df, indexes, test_rate=0.1, valid_rate=0.1, shuffle=True):
+        df_train, df_valid, df_test = None, None, None
+        for index in indexes:
+            df_part = df[index]
+            if shuffle:
+                df_part = df_part.sample(frac=1)
+
+            n_test = max(1, int(len(df_part) * test_rate))
+            n_valid = max(1, int(len(df_part) * valid_rate))
+            n_train = len(df_part) - n_test - n_valid
+            # print('n_train/n_valid/n_test:', n_train, n_valid, n_test)
+            if df_train is None:
+                df_train = df_part[:n_train]
+            else:
+                df_train = df_train.append(df_part[:n_train])
+
+            if df_valid is None:
+                df_valid = df_part[n_train:n_train + n_valid]
+            else:
+                df_valid = df_valid.append(df_part[n_train:n_train + n_valid])
+
+            if df_test is None:
+                df_test = df_part[n_train + n_valid:]
+            else:
+                df_test = df_test.append(df_part[n_train + n_valid:])
+
+        return df_train, df_valid, df_test
+
+    @staticmethod
+    def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=1):
+        """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+        lr = init_lr * (0.1 ** (epoch // lr_decay_epoch))
+
+        if epoch % lr_decay_epoch == 0:
+            # print('LR is set to {}'.format(lr))
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+
+        return optimizer
+
+
+if __name__ == '__main__':
+    pass
