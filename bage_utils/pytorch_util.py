8192e98cde06816b8d6d05d7a5fa30b19eb75b2a
Hyewoong Park <bage79@gmail.com>
2017-11-16 17:15:35 +0900

    ..

--- bage_utils/pytorch_util.py
+++ bage_utils/pytorch_util.py
@@ -1,18 +1,36 @@
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+# import torch.nn as nn
+# from torch.autograd import Variable
+# from torch.utils.data import DataLoader
+# from torchvision import datasets, transforms
+import numpy as np
+import pandas as pd
 
 
 class PytorchUtil(object):
+    random_seed = 7942
+
+    @classmethod
+    def init_random_seed(cls, random_seed=None):
+        if random_seed is None:
+            random_seed = cls.random_seed
+
+        np.random.seed(random_seed)
+        torch.manual_seed(random_seed)
+        torch.cuda.manual_seed_all(random_seed)
+
     @staticmethod
-    def equal_distribution(df, indexes, test_rate=0.1, valid_rate=0.1, shuffle=True):
+    def equal_distribution(df, indexes: pd.Series, test_rate=0.1, valid_rate=0.1, shuffle=True):
         df_train, df_valid, df_test = None, None, None
+        sums = [i.sum() for i in indexes]
+        min_count = min(sums)
+        # print('min: %s in %s' % (min_count, sums))
+
         for index in indexes:
             df_part = df[index]
+            df_part = df_part[:min_count]
             if shuffle:
-                df_part = df_part.sample(frac=1)
+                df_part = df_part.sample(frac=1, random_state=7942)
 
             n_test = max(1, int(len(df_part) * test_rate))
             n_valid = max(1, int(len(df_part) * valid_rate))
@@ -36,7 +54,7 @@ class PytorchUtil(object):
         return df_train, df_valid, df_test
 
     @staticmethod
-    def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=1):
+    def exp_learing_rate_decay(optimizer, epoch, init_lr=0.001, lr_decay_epoch=1):
         """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
         lr = init_lr * (0.1 ** (epoch // lr_decay_epoch))
 
