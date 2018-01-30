bab7b494169a973844c74e64b9620a2ee3e61dae
Hyewoong Park <bage@smfcast.com>
2018-01-30 14:27:45 +0900

    ..

--- bage_utils/pytorch_util.py
+++ bage_utils/pytorch_util.py
@@ -37,17 +37,14 @@ class PytorchUtil(object):
                 torch.cuda.manual_seed_all(random_seed)
         return random_seed
 
-    @classmethod
-    def exp_learing_rate_decay(cls, optimizer, epoch, init_lr=0.001, lr_decay_epoch=1) -> torch.optim.Optimizer:
-        """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
-        lr = init_lr * (0.1 ** (epoch // lr_decay_epoch))
-
-        if epoch % lr_decay_epoch == 0:
-            # print('LR is set to {}'.format(lr))
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = lr
-
-        return optimizer
+    # @classmethod
+    # def exp_learing_rate_decay(cls, optimizer, epoch, init_lr=0.001, lr_decay_epoch=1) -> torch.optim.Optimizer:
+    #     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+    #     if epoch >= lr_decay_epoch:
+    #         for param_group in optimizer.param_groups:
+    #             param_group['lr'] = init_lr * (0.1 ** (epoch // lr_decay_epoch))
+    #
+    #     return optimizer
 
     # noinspection PyDefaultArgument
     @classmethod
