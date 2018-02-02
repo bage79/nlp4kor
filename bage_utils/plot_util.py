9e22339a71694ce7b18dd548a5cad6b692bd1e86
Hyewoong Park <bage@smfcast.com>
2018-02-02 17:40:26 +0900

    ..

--- bage_utils/plot_util.py
+++ bage_utils/plot_util.py
@@ -38,16 +38,14 @@ class PlotUtil(object):
         fig.suptitle(title, size=title_font_size)
         # print(f'figsize: {figsize_pixel}, total_subplot:{total_subplot}, plot_filepath:{plot_filepath}')
 
-        if y_min_max is None:
-            y_min_max = min(df.min()), max(df.max())
-
         if len(subtitles) < len(df.columns):
             subtitles = [str(col) for col in df.columns]
         for nth, col in enumerate(df.columns):
             if debug:
                 print(f'plot {nth} th...')
             ax = pyplot.subplot(gs[nth + plot_columns])
-            ax.set_ylim(y_min_max)
+            if y_min_max is not None:
+                ax.set_ylim(y_min_max)
             if axhline:
                 ax.axhline(y=0.0, color='black', linestyle=':')
             pyplot.setp(ax.xaxis.get_majorticklabels(), rotation=rotate_xtick)
@@ -77,8 +75,6 @@ class PlotUtil(object):
             if os.path.exists(plot_filepath):
                 os.remove(plot_filepath)
             pyplot.savefig(plot_filepath)
-
-        return y_min_max
 
 
 if __name__ == '__main__':
