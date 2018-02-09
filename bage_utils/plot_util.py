ab77402ff157f477e3633949722f65fef022aea8
Hyewoong Park <bage@smfcast.com>
2018-02-09 11:22:01 +0900

    multiple base columns

--- bage_utils/plot_util.py
+++ bage_utils/plot_util.py
@@ -26,14 +26,13 @@ class PlotUtil(object):
         # return matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
 
     @staticmethod
-    def grid_plots(df: pandas.DataFrame, columns=None, base_column=None, title='', subtitles=[], kind='line', y_min_max=None, y_label='', plot_columns=1, max_xticks=4, rotate_xtick=45, one_row_height=400, width=2048, title_font_size=50, axhline=True, secondary_y=False, plot_filepath=None, debug=False):
+    def grid_plots(df: pandas.DataFrame, columns=None, base_columns=[], title='', subtitles=[], kind='line', y_min_max=None, y_label='', plot_columns=1, max_xticks=4, rotate_xtick=45, one_row_height=400, width=2048, title_font_size=50, axhline=True, secondary_y=False, plot_filepath=None, debug=False):
         matplotlib.rcParams['legend.loc'] = 'upper left'
 
         if columns is None:
             columns = list(df.columns)
 
-        if base_column is not None and base_column in columns:
-            columns.remove(base_column)
+        print('base_columns:', base_columns)
 
         plot_rows = math.ceil(len(columns) / plot_columns) + 1  # with title
         figsize_pixel = (width, one_row_height * plot_rows)
@@ -72,8 +71,9 @@ class PlotUtil(object):
                     print(sub_df.head())
                 sub_df['pos'].plot.bar(color='r', title=subtitles[nth])
                 sub_df['neg'].plot.bar(color='b')
-                if base_column is not None:
-                    df[base_column].plot.bar(color='g')  # TODO: TEST
+                if len(columns) == len(base_columns):
+                    print(base_columns[nth])
+                    df[base_columns[nth]].plot.bar(color='g')  # TODO: TEST
                 if debug:
                     print('xticks:', len(xticks), xticks)
                 # print([df.index[idx] for idx in xticks])
@@ -82,8 +82,8 @@ class PlotUtil(object):
                 if debug:
                     print(sub_df.head())
                 sub_df[col].plot.line(title=subtitles[nth], xticks=xticks)
-                if base_column is not None:
-                    df[base_column].plot.line(color='g', secondary_y=secondary_y)
+                if len(columns) == len(base_columns):
+                    df[base_columns[nth]].plot.line(color='g', secondary_y=secondary_y)
             pyplot.legend()
 
         fig.tight_layout()
